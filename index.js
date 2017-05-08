aa0b7c734ee83e94e7a9585d5efb2170e1a624f4
Gustavo Otero <gustavo@chimera.digital>
2017-05-07 19:18:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Resource = require('deployd/lib/resource'),
   util = require('util'),
   nodemailer = require('nodemailer'),
-  smtpTransport = require('nodemailer-smtp-transport');
+  smtpTransport = require('nodemailer-smtp-transport')
 
 
 function Mail(name, options) {
@@ -10,20 +10,26 @@ function Mail(name, options) {
   var authParams = null;
   //If an empty string needs to be passed as username, use the environment variable
   if (this.config.username !== '') {
-      authParams={
-        user: this.config.username,
-        pass: this.config.password
-      };
+    authParams = {
+      user: this.config.username,
+      pass: this.config.password
+    };
   }
 
   this.transport = nodemailer.createTransport(smtpTransport({
-    host : this.config.host || "localhost",
-    port : parseInt(this.config.port, 10) || 25,
-    secure : this.config.ssl,
-    auth : authParams
-  }))
+    host: this.config.host || "localhost",
+    port: parseInt(this.config.port, 10) || 25,
+    secure: this.config.ssl,
+    auth: authParams,
+    requireTLS: true,
+    tls: {
+      rejectUnauthorized: false
+    }
+  }));
 
-//this.transport.use('compile', htmlToText({}) );
+
+
+  //this.transport.use('compile', htmlToText({}) );
 }
 util.inherits(Mail, Resource); //aplicar herencia al recurso
 module.exports = Mail; //generar el recurso
@@ -33,80 +39,107 @@ Mail.prototype.clientGeneration = true; //habilitar al cliente para generar el r
 
 //Se crea el panel de control con los campos para guardar en configuracion
 Mail.basicDashboard = {
-  settings: [
-  {
-    name        : 'host',
-    type        : 'text',
-    description : 'Host name of your SMTP provider. Defaults to \'localhost\'.'
+  settings: [{
+    name: 'host',
+    type: 'text',
+    description: 'Host name of your SMTP provider. Defaults to \'localhost\'.'
   }, {
-    name        : 'port',
-    type        : 'number',
-    description : 'Port number of your SMTP provider. Defaults to 25'
+    name: 'port',
+    type: 'number',
+    description: 'Port number of your SMTP provider. Defaults to 25'
   }, {
-    name        : 'ssl',
-    type        : 'checkbox',
-    description : 'Use SSL.'
+    name: 'ssl',
+    type: 'checkbox',
+    description: 'Use SSL.'
   }, {
-    name        : 'username',
-    type        : 'text',
-    description : 'SMTP username.'
+    name: 'username',
+    type: 'text',
+    description: 'SMTP username.'
   }, {
-    name        : 'password',
-    type        : 'text',
-    description : 'SMTP password.'
+    name: 'password',
+    type: 'text',
+    description: 'SMTP password.'
   }, {
-    name        : 'defaultFromAddress',
-    type        : 'text',
-    description : 'Optional; if not provided you will need to provide a \'from\' address in every request'
-  },
-  {
-    name        : 'internalOnly',
-    type        : 'checkbox',
-    description : 'Only allow internal scripts to send email'
-}]
+    name: 'defaultFromAddress',
+    type: 'text',
+    description: 'Optional; if not provided you will need to provide a \'from\' address in every request'
+  }, {
+    name: 'internalOnly',
+    type: 'checkbox',
+    description: 'Only allow internal scripts to send email'
+  }, {
+    name: 'secret',
+    type: 'text',
+    description: 'secret key or password to make external requests, only for external calls, will be ignored for internal calls'
+  }]
 };
 
 
-Mail.prototype.handle = function ( ctx, next ) { //Comportamiento al llamar funcion
-  
-  if ( ctx.req && ctx.req.method !== 'POST' ) {
+Mail.prototype.handle = function(ctx, next) { //Comportamiento al llamar funcion
+
+  if (ctx.req && ctx.req.method !== 'POST') {
     return next();
   }
 
+
   //check for internal only flag
-  if ( !ctx.req.internal && this.config.internalOnly ) {
-    return ctx.done({ statusCode: 403, message: 'Forbidden' });
+  if (!ctx.req.internal && this.config.internalOnly) {
+    return ctx.done({
+      statusCode: 403,
+      message: 'Forbidden'
+    });
   }
+
+  //check for secretKey
+  if (this.config.secret !== ctx.body.secret && !ctx.req.internal) {
+    {
+      return ctx.done({
+        statusCode: 401,
+        message: 'Unauthorized: Access is denied due to invalid credentials'
+      });
+    }
+  }
+
+
 
   var options = ctx.body;
   options.from = options.from || this.config.defaultFromAddress;
 
   var errors = {};
-  if ( !options.to ) {
+  if (!options.to) {
     errors.to = '\'to\' is required';
   }
-  if ( !options.from ) {
+  if (!options.from) {
     errors.from = '\'from\' is required';
   }
-  if ( !options.text && !options.html ) {
+  if (!options.text && !options.html) {
     errors.text = '\'text\' or \'html\' is required';
   }
-  
-  if ( Object.keys(errors).length ) {
-    return ctx.done({ statusCode: 400, errors: errors });
+
+  if (Object.keys(errors).length) {
+    return ctx.done({
+      statusCode: 400,
+      errors: errors
+    });
   }
 
   // trim
   options.subject = options.subject.trim();
   options.text = options.text.trim();
 
-  this.transport.sendMail(
+  var that = this;
+
+  that.transport.sendMail(
     options,
-    function( err, response ) {
-      if ( err ) {
-        return ctx.done( err );
+    function(err, response) {
+      if (err) {
+        return ctx.done(options.html);
       }
-      ctx.done( null, { message : response.message } );
+      ctx.done(null, {
+        message: response.message
+      });
     }
   );
+ 
+
 }
